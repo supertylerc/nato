79ac4b73251edd1ed8448ca2627048c83d4852b5
Tyler Christiansen <tyler@oss-stack.io>
2014-06-25 19:47:26 -0700

    correct issues identified by scrutinizer

--- Rakefile
+++ Rakefile
@@ -1,5 +1,6 @@
+# encoding: UTF-8
 require 'minitest'
-task default: %w[test]
+task default: %w(test)
 
 task :test do
   ruby 'test/nato.rb'
--- test/nato.rb
+++ test/nato.rb
@@ -1,10 +1,15 @@
+# encoding: UTF-8
 require_relative '../nato'
 require 'minitest/autorun'
 
 class TestNato < Minitest::Test
+
+  # Creates a new Nato object
   def setup
     @nato = Nato.new
   end
+
+  # Runs the #translate method to match it against 'f1u2c3k{4'
   def test_nato
     assert_equal 'Foxtrot 1Uniform 2Charlie 3Kilo {4', @nato.translate('f1u2c3k{4')
   end
